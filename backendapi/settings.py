1c3d70634ee6ff890569904c87ecc0575fb7ec7e
sufyank90 <sufyanshahidaptech@gmail.com>
2024-06-09 18:03:56 +0500

    vercel new update

--- backendapi/settings.py
+++ backendapi/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 echo "BUILD START"
+# Install dependencies from requirements.txt
 python3.9 -m pip install -r requirements.txt
+
+# Ensure Django is installed and collect static files
+python3.9 -m pip install django  # Install Django if not already installed
 python3.9 manage.py collectstatic --noinput --clear
+
 echo "BUILD END"
