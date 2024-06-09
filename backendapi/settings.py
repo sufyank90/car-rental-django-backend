2b3d088cac6af0c802ecbefa94fd41fd7900fcb0
sufyank90 <sufyanshahidaptech@gmail.com>
2024-06-10 02:57:29 +0500

    Set up Django backend for car rental application

--- backendapi/settings.py
+++ backendapi/settings.py
@@ -83,19 +83,19 @@ WSGI_APPLICATION = 'backendapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'rentalcar',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',  # Or your MySQL host
-#         'PORT': '3306',
-#         'OPTIONS': {  
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-#         }
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rentalcar',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Or your MySQL host
+        'PORT': '3306',
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }
+    }
+}
 
 
 # Password validation
